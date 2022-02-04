9dd781ffd8e9d3bfad38b02d046353f5304b1e90
Mike Nieva <m@nieva.team>
2022-02-04 10:07:59 -0600

    Formulario y página individual terminada

--- routes/books.js
+++ routes/books.js
@@ -11,6 +11,13 @@ router.get("/", bookController.getBooks)
 // CREAR PÁGINA PARA CREAR UN LIBRO EN BD
 router.get("/create", bookController.createBooks)
 
+// ENVIAR DATOS DE FORMULARIO PARA CREAR LIBRO EN BD
+router.post("/create", bookController.createBooksForm)
+
+
+// CREAR UNA PÁGINA INDIVIDUAL PARA CADA LIBRO CON LOS DATOS RESPEECTIVOS
+// http://localhost:3005/books/1357968416316574654654
+router.get("/:bookID", bookController.getSingleBook)
 
 
 module.exports = router
